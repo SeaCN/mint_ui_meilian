c0809e4a16ba2c64e59fc975ee2dd56a0989ca28
Sea Joe <275908541@qq.com>
2018-06-02 12:11:04 +0800

    double loadmore

--- src/components/test.vue
+++ src/components/test.vue
@@ -7,17 +7,32 @@
     <mt-tab-container v-model="selected">
       <mt-tab-container-item id="1">
         <mt-loadmore :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore" :autoFill="autoFill">
-          <ul>
-            <li v-for="item in list">{{ item }}</li>
-          </ul>
+          <mt-cell-swipe v-for="item in list" :key='item.id'
+                         :title="item.title" :label="item.createTime"
+                         @click.native="showDetail"
+                         :right="[{
+            content: '回复',
+            style: { background: '#6eff2a', color: '#fff' },
+            handler: reply
+          }]"
+          >
+            <input id="itemC" type="hidden" v-model="item.content">
+            <input id="itemI" type="hidden" v-model="item.img">
+            <input id="itemId" type="hidden" v-model="item.id">
+          </mt-cell-swipe>
         </mt-loadmore>
       </mt-tab-container-item>
 
       <mt-tab-container-item id="2">
         <mt-loadmore :bottom-method="loadBottom1" :bottom-all-loaded="allLoaded1" ref="loadmore1" :autoFill="autoFill1">
-          <ul>
-            <li v-for="item in list1">{{ item }}</li>
-          </ul>
+          <mt-cell-swipe v-for="item in fblist" :key='item.id'
+                         :title="item.title" :label="item.createTime"
+                         @click.native="fbShowDetail"
+          >
+            <input id="itemCC" type="hidden" v-model="item.content">
+            <input id="itemII" type="hidden" v-model="item.img">
+            <input id="itemIdd" type="hidden" v-model="item.id">
+          </mt-cell-swipe>
         </mt-loadmore>
       </mt-tab-container-item>
     </mt-tab-container>
@@ -27,60 +42,173 @@
 </template>
 
 <script>
+  import Constant from '../assets/constant.js'
+  import axios from "axios"
     export default {
       name: "",
       data(){
         return{
           selected: "1",
-          list: [1,2,3,4,5,6,7,8,9,0],
-          list1: [1,2,3,4,5,6,7,8,9,0],
+          list: [],
+          fblist: [],
           count: 0,
           count1: 0,
           autoFill: false,
           autoFill1: false,
           allLoaded: false,
-          allLoaded1: false
+          allLoaded1: false,
+          currentPage: 1,
+          pageSize: 10,
+          dynamicDiv: {},
+          fbdynamicDiv: {},
+          fbpageSize: 10,
+          fbcurrentPage: 1
         }
       },
       methods: {
-        loadBottom: function() {console.log("触发")
+        loadBottom: function() {
           this.loadPageList();
           this.$refs.loadmore.onBottomLoaded();// 固定方法，查询完要调用一次，用于重新定位
         },
-        loadBottom1: function() {console.log("触发1")
+        loadBottom1: function() {
           this.loadPageList1();
           this.$refs.loadmore1.onBottomLoaded();// 固定方法，查询完要调用一次，用于重新定位
         },
         loadPageList:function (){
-          // 查询数据
-          /*this.api.PageList(this.searchCondition).then(data =>{
-            // 是否还有下一页，加个方法判断，没有下一页要禁止上拉
-            this.isHaveMore(data.result.haveMore);
-            this.pageList = data.result.pageList;
-            this.$nextTick(function () {
-              // 原意是DOM更新循环结束时调用延迟回调函数，大意就是DOM元素在因为某些原因要进行修改就在这里写，要在修改某些数据后才能写，
-              // 这里之所以加是因为有个坑，iphone在使用-webkit-overflow-scrolling属性，就是移动端弹性滚动效果时会屏蔽loadmore的上拉加载效果，
-              // 花了好久才解决这个问题，就是用这个函数，意思就是先设置属性为auto，正常滑动，加载完数据后改成弹性滑动，安卓没有这个问题，移动端弹性滑动体验会更好
-              this.scrollMode = "touch";
-            });
-          });*/
-          if (this.count > 200){
-            this.allLoaded = true
+          $.ajax({
+            url: Constant.path + "/suggestion/suggestionByPage",
+            data: {
+              currentPage: this.currentPage++,
+              pageSize: this.pageSize
+            },
+            type: "post",
+            dataType: "json",
+            async: false,
+            success: (response)=> {
+              if(response.data.length != 0){
+                this.list = this.list.concat(response.data)
+              }else{console.log("over")
+                this.allLoaded = true
+                this.$toast({
+                  message: '暂无数据',
+                  duration: 2000,
+                  className: "over"
+                });
+              }
+            }
+          })
+        },
+        reply: function () {
+          let closeA = $(event.target).closest('a.mint-cell')
+          let itemid = closeA.find('#itemId').val()
+          this.$router.push({path: 'Reply', query: {suggesstionid: itemid}})
+        },
+        showDetail: function (e) {
+          let closeA = $(e.target).closest('a.mint-cell')
+          if(closeA.find('div.animateDiv').length !== 0
+            && closeA.find('div.animateDiv').css('display') != 'none'){
+            this.dynamicDiv.hide()
             return;
           }
-          for(var i=0; i<10; i++){
-            this.list.push(++this.count)
-          }
+          this.dynamicDiv.hide()
+          this.dynamicDiv.html("")
+          this.dynamicDiv.append($('<p></p>'))
+          this.dynamicDiv.find('p').html(closeA.find('#itemC').val())
+          let imgdata = closeA.find('#itemI').val()
+          this.dynamicDiv.find('p').after($("<p><img src='" + imgdata +"'/></p>"))
+          closeA.append(this.dynamicDiv)
+          this.dynamicDiv.fadeIn(1000)
         },
         loadPageList1:function (){
-          if (this.count1 > 200){
-            this.allLoaded1 = true
+          $.ajax({
+            url: Constant.path + "/feedback/selectByUserid",
+            data: {
+              currentPage: this.fbcurrentPage++,
+              pageSize: this.fbpageSize,
+              userId: 1
+            },
+            type: "post",
+            dataType: "json",
+            async: false,
+            success: (response)=> {
+              if(response.data.length != 0){console.log(response.data)
+                this.fblist = this.fblist.concat(response.data)
+              }else{
+                this.allLoaded = true
+                this.$toast({
+                  message: '暂无数据',
+                  duration: 2000,
+                  className: "over"
+                });
+              }
+            }
+          })
+        },
+        fbShowDetail: function (e) {
+          let closeA = $(e.target).closest('a.mint-cell')
+          if(closeA.find('div.animateDiv').length !== 0
+            && closeA.find('div.animateDiv').css('display') != 'none'){
+            this.fbdynamicDiv.hide()
             return;
           }
-          for(var i=0; i<10; i++){
-            this.list1.push(++this.count1)
-          }
+          this.fbdynamicDiv.hide()
+          this.fbdynamicDiv.html("")
+          this.fbdynamicDiv.append($('<p></p>'))
+          this.fbdynamicDiv.find('p').html(closeA.find('#itemCC').val())
+          let imgdata = closeA.find('#itemII').val()
+          this.fbdynamicDiv.find('p').after($("<p><img src='" + imgdata +"'/></p>"))
+          closeA.append(this.fbdynamicDiv)
+          this.fbdynamicDiv.fadeIn(1000)
         }
+      },
+      created: function () {
+        /*建议列表初始化*/
+        this.dynamicDiv = $('<div class="animateDiv"></div>');
+        this.dynamicDiv.css({'display': 'none', 'padding': '0 10px', 'font-style': 'italic', 'font-family': 'arial, sans-serif'})
+        this.dynamicDiv.slideUp()
+        $.ajax({
+          url: Constant.path + '/suggestion/suggestionByPage',
+          type: 'POST',
+          async: false,
+          dataType: 'json',
+          data: {
+            currentPage: this.currentPage++,
+            pageSize: this.pageSize
+          },
+          success: (response)=>{
+            if(response.code === "200"){
+              this.list = response.data
+            }
+          }
+        })
+
+        /*回复列表初始化*/
+        this.fbdynamicDiv = $('<div class="animateDiv"></div>');
+        this.fbdynamicDiv.css({'display': 'none', 'padding': '0 10px', 'font-style': 'italic', 'font-family': 'arial, sans-serif'})
+        this.fbdynamicDiv.slideUp()
+        $.ajax({
+          url: Constant.path + "/feedback/selectByUserid",
+          data: {
+            currentPage: this.fbcurrentPage++,
+            pageSize: this.fbpageSize,
+            userId: 1
+          },
+          type: "post",
+          dataType: "json",
+          async: false,
+          success: (response)=> {
+            if(response.data.length != 0){console.log(response.data)
+              this.fblist = this.fblist.concat(response.data)
+            }else{
+              this.allLoaded = true
+              this.$toast({
+                message: '暂无数据',
+                duration: 2000,
+                className: "over"
+              });
+            }
+          }
+        })
       }
     }
 </script>
